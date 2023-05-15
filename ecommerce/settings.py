027a33b1a792bfe1b6fafa58937f6150221a5458
akshay343 <127043464+Akshay-343@users.noreply.github.com>
2023-05-15 13:00:17 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -125,8 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [STATIC_DIR , MEDIA_ROOT ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, MEDIA_ROOT]
+STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
+
 
 
 
